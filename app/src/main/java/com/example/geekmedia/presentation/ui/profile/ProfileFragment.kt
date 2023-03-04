76a78a0968655c64b27410d9fee6db73e4acec76
FATboy331 <alexkox74@gmail.com>
2023-03-04 15:36:12 +0600

    Almost all

--- app/src/main/java/com/example/geekmedia/presentation/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/geekmedia/presentation/ui/profile/ProfileFragment.kt
@@ -2,7 +2,7 @@ package com.example.geekmedia.presentation.ui.profile
 
 import android.view.LayoutInflater
 import com.example.geekmedia.databinding.FragmentProfileBinding
-import com.example.geekmedia.presentation.base.BaseFragment
+import com.example.geekmedia.presentation.ui.base.BaseFragment
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
     override fun inflateViewBinding(inflater: LayoutInflater): FragmentProfileBinding {
